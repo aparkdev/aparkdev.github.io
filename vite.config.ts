b9bdf1b5d90806cd7713e83f3b6a163a2a223410
albertpark <albert_park@hotmail.com>
2025-10-30 20:17:32 -0700

    Fix basebath in vite and jekyll config

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,10 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  base: mode === 'production' ? '/petal-grid-store/' : '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  base: '/', //mode === 'production' ? '/petal-grid-store/' : '/',
 }));
