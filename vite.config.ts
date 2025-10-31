106f1440b265f27d2fb655e333a20ce424866a63
albertpark <albert_park@hotmail.com>
2025-10-30 17:33:00 -0700

    Fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ }) => ({
-  plugins: [react()],
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   base: '/petal-grid-store/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./petal-grid-store/src"),
+    },
+  },
 }));
